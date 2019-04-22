647f28406aaf135666249211afa8775caa340a53
AlexScherbackov <alsherbakov13@gmail.com>
2019-04-22 20:00:20 +0400

    add filmLoading to store

--- src/store/store.js
+++ src/store/store.js
@@ -1,14 +1,22 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 
-Vue.use(Vuex); 
+Vue.use(Vuex);
 
 export default new Vuex.Store({
 	state:{
 		people: [],
+		planets: [],
+		films: []
 	},
 	getters: {
 		people: state => state.people,
+		planets: state => state.planets,
+		films: state => state.films,
+		film: state => url => {
+			return state.films.filter(item => item.url == url)
+		},
+
 	},
 	actions: {
 		loadPeople(context){
@@ -23,10 +31,24 @@ export default new Vuex.Store({
 				});
 		
 		},
+		loadFilms(context){
+			const url = 'https://swapi.co/api/films/?format=json';
+
+			axios.get(url)
+				.then((response) => {
+					context.commit('loadFilms', response.data.results)
+				})
+				.catch(error => {
+					console.log(error);
+				});
+		}
 	},
 	mutations: {
 		loadPeople(state, payload){
 			state.people = payload;
 		},
+		loadFilms(state, payload){
+			state.films = payload
+		}
 	}
 })
