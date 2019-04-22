f57261c4f737ac795bc4623dad6cc0c3d0f91590
AlexScherbackov <alsherbakov13@gmail.com>
2019-04-22 20:26:03 +0400

    star wars list

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,71 @@
 <template>
   <div id="app">
-   
+    <star-wars-view 
+      :tableData="tableData" 
+      v-if="isLoaded" 
+    />
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app',
-  data () {
-    return {
-     
+  import {mapActions} from 'vuex';
+  import {mapGetters} from 'vuex';
+
+  import StarWarsView from './components/StarWarsView.vue';
+
+  export default {
+    name: 'app',
+    components: {
+      StarWarsView
+    },
+    data () {
+      return {
+        tableData: [],
+        isLoaded: false
+      }
+    },
+    created(){
+      const loadPeoplePromise = new Promise((resolve, reject) => {
+        resolve()
+      });
+      loadPeoplePromise
+        .then(
+          result => this.loadFilms(),
+          error => console.log(error)
+        )
+        .then(
+          result => this.loadPeople()
+        )
+    },
+    computed:{
+      ...mapGetters(['people', 'films']),
+    },
+     methods:{
+      ...mapActions(['loadPeople', 'loadFilms']),
+    },
+    watch:{
+      people:{
+        immediate: true,
+        handler(){
+          this.tableData = this.people;
+          if(this.tableData.length > 0){
+            this.isLoaded = true
+          }
+        }
+      },
+      films:{
+        handler(){
+          this.tableData = this.tableData.map(item => {
+            item.films = item.films.map( film => {
+              if(film)
+                return this.$store.getters.film(film)[0].title;  
+            });
+            return item;
+          })
+        }
+      }
     }
   }
-}
 </script>
 
 <style lang="scss">
---
+++ src/components/StarWarsView.vue
@@ -0,0 +1,63 @@
+<template>
+	<el-container>
+		<el-table
+			:data="tableData"
+			class="starWars">
+			<el-table-column type="expand">
+				<template slot-scope="props">
+					<p>Films:</p>
+					<ul>
+						<li v-for="item in props.row.films">{{item}}</li>
+					</ul>
+				</template>
+			</el-table-column>
+			<el-table-column
+				label="Name"
+				prop="name">
+			</el-table-column>
+			<el-table-column
+				label="Birthday"
+				prop="birth_year">
+			</el-table-column>
+			<el-table-column
+				label="Eyes"
+				prop="eye_color">
+			</el-table-column>
+			<el-table-column
+				label="Gender"
+				prop="gender">
+			</el-table-column>
+			<el-table-column
+				label="Hair"
+				prop="hair_color">
+			</el-table-column>
+			<el-table-column
+				label="Height"
+				prop="height">
+			</el-table-column>
+		</el-table>
+	</el-container>
+</template>
+<script>
+	import {mapGetters} from 'vuex';
+
+	export default {
+		props:{
+			tableData:  {
+				type: Array,
+				requered: true
+			}
+		},
+		data(){
+			return{
+				starwarsData: []
+			}
+		},
+		mounted(){
+			
+		},
+	}
+</script>
+<style lang="scss" scoped>
+
+</style>
